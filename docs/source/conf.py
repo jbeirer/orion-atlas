078138eecbd8abbc1f244bc8c109f603f61123d9
jbeirer <39070576+jbeirer@users.noreply.github.com>
2022-02-07 22:28:45 +0000

    Change project name

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Orion@ATLAS'
+copyright = '2022, The ATLAS Collaboration'
+author = 'The ATLAS Collaboration'
 
 release = '0.1'
 version = '0.1.0'
